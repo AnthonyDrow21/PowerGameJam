ad19ba9f2fa24c9306a51f7b9429762fe206df63
AnthonyDrow <anthonyedrow@gmail.com>
2025-02-11 18:50:19 -0500

    Fixed slimes interacting with edges of floors.

--- Digdrop/Scenes/Enemy/Greenslime.gd
+++ Digdrop/Scenes/Enemy/Greenslime.gd
@@ -15,9 +15,9 @@ const goopGuck = preload("res://Scenes/Enemy/goopGuck.tscn");
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if ray_cast_right.is_colliding():
+	if ray_cast_right.is_colliding() == true or $RayCastDownRight.is_colliding() == false:
 		direction = -1
-	if ray_cast_left.is_colliding():
+	if ray_cast_left.is_colliding() == true or $RayCastDownLeft.is_colliding() == false:
 		direction = 1
 	position.x += SPEED * delta * direction
 	
