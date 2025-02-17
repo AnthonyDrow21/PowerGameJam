221e8be9465c07cc3d7c77641891755c56c65802
AnthonyDrow <anthonyedrow@gmail.com>
2025-02-17 14:47:14 -0500

    Allowed battery charge amount to be modified. Allowed green slime direction to be changed. Updated Map.

--- Digdrop/Scenes/BatteryPack.gd
+++ Digdrop/Scenes/BatteryPack.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
+@export var chargeAmount = 5.0;
+
 func _on_body_entered(body: Node2D) -> void:
 	# Don't allow the player to gain more energy than their max energy.
 	print("Energy Restored");
-	PlayerStats.DrillEnergy = min(PlayerStats.DrillEnergy + 5.0, PlayerStats.MaxEnergy);
+	PlayerStats.DrillEnergy = min(PlayerStats.DrillEnergy + chargeAmount, PlayerStats.MaxEnergy);
 	queue_free()
--- Digdrop/Scenes/Enemy/Greenslime.gd
+++ Digdrop/Scenes/Enemy/Greenslime.gd
@@ -2,11 +2,14 @@ extends Node2D
 
 var SPEED = 20
 
-var direction = -1
+# This should be 1 or -1.
+@export var direction = -1
 
 @onready var player = get_parent().get_node("player")
 
 const goopGuck = preload("res://Scenes/Enemy/goopGuck.tscn");
+
+@export var ignoreFloor = false;
 
 @onready var game = get_tree().get_root();
 @onready var ray_cast_left = $RayCastLeft
@@ -15,9 +18,11 @@ const goopGuck = preload("res://Scenes/Enemy/goopGuck.tscn");
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if ray_cast_right.is_colliding() == true or $RayCastDownRight.is_colliding() == false:
+	var floorcheckRight = $FloorCheckRight.is_colliding() == false and ignoreFloor == false;
+	var floorcheckLeft = $FloorCheckLeft.is_colliding() == false and ignoreFloor == false;
+	if ray_cast_right.is_colliding() == true or floorcheckRight == true:
 		direction = -1
-	if ray_cast_left.is_colliding() == true or $RayCastDownLeft.is_colliding() == false:
+	if ray_cast_left.is_colliding() == true or floorcheckLeft == true:
 		direction = 1
 	position.x += SPEED * delta * direction
 	
